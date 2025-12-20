537a708cc9e17ec37c80e43fd228cd4d293d62a5
volodymyr.y.savchenko <volodymyr.y.savchenko@gmail.com>
2025-12-20 23:50:37 +0200

    SPLIT ALL FILES TO PRACTICE AND THEOTY FOLDERS

---
+++ Practice/try-draft444/try-draft444/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+class Program
+{
+    static public void function(int a, int b)
+    {
+        int c = a + b;
+        string f = c.ToString();
+
+        Console.WriteLine(f);
+    }
+
+    static void Main(string[] args)
+    {
+        Action<int, int> RefToFunc = (int a, int b) =>
+        {
+            int c = a + b;
+            string f = c.ToString();
+
+            Console.WriteLine(f);
+        };
+
+        RefToFunc(4, 6);
+        
+    }
+}
